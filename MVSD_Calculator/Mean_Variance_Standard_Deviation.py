9f482e6e0693b0dc36cce558a79ac6fb502479c9
Z3ZTRON <aydexter05@gmail.com>
2025-03-02 20:13:53 +0000

    Update Mean_Variance_Standard_Deviation.py

--- MVSD_Calculator/Mean_Variance_Standard_Deviation.py
+++ MVSD_Calculator/Mean_Variance_Standard_Deviation.py
@@ -1,1 +1,21 @@
 
+import numpy as np
+
+def calculator(array):
+    if len(array) != 9:
+        raise ValueError("The array must contain nine numbers. ") # 
+    
+    matrix = np.array(array).reshape((3,3)) # Turn the list to an array
+    
+    compile_calc = {
+        'mean':[matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), np.mean(matrix).tolist()],
+        'variance':[matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), np.var(matrix).tolist()],
+        'standard deviation':[matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), np.std(matrix).tolist()],
+        'max':[matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), np.max(matrix).tolist()],
+        'min':[matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), np.min(matrix).tolist()],
+        'sum':[matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), np.sum(matrix).tolist()]
+    }
+    
+    return compile_calc
+
+print(calculator([2,6,2,8,4,0,1,5,7]))
